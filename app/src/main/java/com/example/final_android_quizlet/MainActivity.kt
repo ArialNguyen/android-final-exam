288dc71255913565ac10d630b348ea78e45f3a41
123-advs <vovanchien135@gmail.com>
2023-10-18 18:27:14 +0700

    layout 2

--- app/src/main/java/com/example/final_android_quizlet/MainActivity.kt
+++ app/src/main/java/com/example/final_android_quizlet/MainActivity.kt
@@ -1,28 +1,66 @@
 package com.example.final_android_quizlet
 
-import android.content.Intent
+
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
-import com.example.final_android_quizlet.service.user.UserService
-import com.example.final_android_quizlet.models.User
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
-import androidx.lifecycle.lifecycleScope
 
 
 class MainActivity : AppCompatActivity() {
-    private val myScope = CoroutineScope(Dispatchers.Main) // create coroutine Scope in MainThread
+    private val myScope = CoroutineScope(Dispatchers.Main)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val plusIcon: ImageView = findViewById(R.id.imageView5)
+
+        plusIcon.setOnClickListener {
+            showBottomDialog()
+        }
+    }
+
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.activity_bottom_sheet)
+
+        val createHocPhan: LinearLayout = dialog.findViewById(R.id.create_hoc_phan)
+        val createFolder: LinearLayout = dialog.findViewById(R.id.create_folder)
+        val createClass: LinearLayout = dialog.findViewById(R.id.create_class)
+        val cancelButton: ImageView = dialog.findViewById(R.id.cancelButton)
+
+        createHocPhan.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        createFolder.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        createClass.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
     }
 
     override fun onDestroy() {
