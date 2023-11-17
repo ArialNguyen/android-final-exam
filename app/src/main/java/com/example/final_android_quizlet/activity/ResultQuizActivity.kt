e66baba245905429b4ce285c1d8db676895d86aa
123-advs <vovanchien135@gmail.com>
2023-11-17 23:03:54 +0700

    commit file

---
+++ app/src/main/java/com/example/final_android_quizlet/activity/ResultQuizActivity.kt
@@ -0,0 +1,10 @@
+package com.example.final_android_quizlet.activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.final_android_quizlet.R
+class ResultQuizActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_quiz)
+    }
+}
