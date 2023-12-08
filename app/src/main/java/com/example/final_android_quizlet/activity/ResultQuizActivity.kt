480a8ef8efff0e3ddbd8d8ef1d630aa9db0dabde
123-advs <vovanchien135@gmail.com>
2023-12-08 14:45:57 +0700

    update result

--- app/src/main/java/com/example/final_android_quizlet/activity/ResultQuizActivity.kt
+++ app/src/main/java/com/example/final_android_quizlet/activity/ResultQuizActivity.kt
@@ -1,34 +1,46 @@
 package com.example.final_android_quizlet.activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.final_android_quizlet.R
+import com.example.final_android_quizlet.common.ActionTransition
+import com.example.final_android_quizlet.models.QuizWrite
+import com.example.final_android_quizlet.models.Topic
 import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class ResultQuizActivity:AppCompatActivity() {
+
+    private lateinit var tvTrue: TextView
+    private lateinit var tvFalse: TextView
+    private lateinit var tvTotalTrue: TextView
+    private lateinit var tvTotalFalse: TextView
+    private lateinit var btnNewTest: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result_quiz)
 
+        tvTrue = findViewById(R.id.tvTrue_ResultQuizActivity)
+        tvFalse = findViewById(R.id.tvFalse_ResultQuizActivity)
+        tvTotalTrue = findViewById(R.id.tvTotalTrue_ResultQuizActivity)
+        tvTotalFalse = findViewById(R.id.tvTotalFalse_ResultQuizActivity)
+        btnNewTest = findViewById(R.id.btn_exam_ResultQuizActivity)
+
         val overall = intent.getIntExtra("overall", 0)
         val totalItems = intent.getIntExtra("totalItems", 0)
-        val txExit = findViewById<TextView>(R.id.txExit_quiz)
-        val btnResult = findViewById<LinearLayout>(R.id.btnResult_quiz)
 
-        txExit.setOnClickListener {
+        val totalCorrect = overall
+        val totalIncorrect = totalItems - totalCorrect
+
+        tvTotalTrue.text = totalCorrect.toString()
+        tvTotalFalse.text = totalIncorrect.toString()
+
+        btnNewTest.setOnClickListener {
             finish()
         }
-        btnResult.setOnClickListener {
-            finish()
-        }
 
-        val txResult = findViewById<TextView>(R.id.txResult_quiz)
-        txResult.text = "$overall/$totalItems"
-
-        val circularProgressBar = findViewById<CircularProgressBar>(R.id.circularProgressBar)
-        circularProgressBar.setProgressWithAnimation(overall.toFloat()) // Số câu đúng
-        circularProgressBar.progressMax = totalItems.toFloat() // Tổng số câu
     }
 }
